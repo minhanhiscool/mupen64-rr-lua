af08b0115e708e96da1e2f1200b93d84c44c69bb
minhanhiscoolish <minhanh3mc@gmail.com>
2025-11-17 21:21:33 +0700

    restructurde src and ported dummy plugins

---
+++ src/Unix/Plugins.Video.Dummy/Main.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2025, Mupen64 maintainers, contributors, and original authors (Hacktarux, ShadowPrince, linker).
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "Main.h"
+
+// PlatformService platform_service;
+
+extern "C" void GetSoInfo(core_plugin_info *info)
+{
+    info->ver = 0x0101;
+    info->type = plugin_video;
+    strncpy(info->name, PLUGIN_NAME, std::size(info->name));
+}
+
+extern "C" void SoAbout(void *hParent)
+{
+    const auto msg = PLUGIN_NAME L"\n"
+                                 L"Part of the Mupen64 project family."
+                                 L"\n\n"
+                                 L"https://github.com/mupen64/mupen64-rr-lua";
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, L"About", msg, (SDL_Window *)hParent);
+}
+
+__attribute__((constructor)) void load()
+{
+}
+__attribute__((destructor)) void unload()
+{
+}
